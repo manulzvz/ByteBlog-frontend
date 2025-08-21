2b9aa80112160d3c5da6de70eabcfdabf4ef13c5
manulzvz13 <manulzvz@gmail.com>
2025-08-21 08:56:22 -0600

    feat: Fetch projects with Loader

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,7 +1,19 @@
-const ProjectsPage = () => {
+import type { Route } from "./+types/index";
+
+export async function loader({ request }: Route.LoaderArgs): Promise<any> {
+  const res = await fetch("http://localhost:8000/projects");
+  const data = await res.json();
+
+  return { projects: data };
+}
+
+const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData;
+  console.log(projects);
+
   return (
     <>
-      <h2 className="text-3xl font-bold text-white mb-8">🛠️ My Projects</h2>
+      <h2 className="text-3xl text-white font-bold mb-8">🚀 Projects</h2>
     </>
   );
 };
