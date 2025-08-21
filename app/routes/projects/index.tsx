6a762bcea58f7e753f65b28c36e42af81720dd00
manulzvz13 <manulzvz@gmail.com>
2025-08-21 09:15:39 -0600

    feat: Display loader data

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/index";
 import type { Project } from "~/types";
+import ProjectCard from "~/components/ProjectCard";
 
 export async function loader({
   request,
@@ -12,11 +13,15 @@ export async function loader({
 
 const ProjectsPage = ({ loaderData }: Route.ComponentProps) => {
   const { projects } = loaderData as { projects: Project[] };
-  console.log(projects);
 
   return (
     <>
       <h2 className="text-3xl text-white font-bold mb-8">🚀 Projects</h2>
+      <div className="grid gap-6 sm:grid-cols-2">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </div>
     </>
   );
 };
