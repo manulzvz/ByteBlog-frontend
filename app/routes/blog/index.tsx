526a56bf00200f5d63bfeecdfc6bb642979c7bcc
manulzvz13 <manulzvz@gmail.com>
2025-08-24 17:44:45 -0600

    feat: Add & display PostMeta

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,4 +1,6 @@
 import type { Route } from "./+types";
+import type { PostMeta } from "~/types";
+import PostCard from "~/components/PostCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,11 +9,26 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-const BlogPage = () => {
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL("/posts-meta.json", request.url);
+  const response = await fetch(url.href);
+  if (!response) throw new Error("Failed to fetch data");
+  const data = await response.json();
+
+  return { posts: data };
+}
+
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData;
   return (
-    <>
+    <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
       <h2 className="text-3xl font-bold text-white mb-8">📝 My Blog</h2>
-    </>
+      {posts.map((post) => (
+        <PostCard key={post.slug} post={post} />
+      ))}
+    </div>
   );
 };
 
