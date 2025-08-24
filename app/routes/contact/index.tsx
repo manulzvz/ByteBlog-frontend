39fafce8961ee4ba1ac0fbad62094a62e05e957e
manulzvz13 <manulzvz@gmail.com>
2025-08-24 16:19:38 -0600

    feat: Add Contact Page form

--- app/routes/contact/index.tsx
+++ app/routes/contact/index.tsx
@@ -9,11 +9,71 @@ export function meta({}: Route.MetaArgs) {
 
 const ContactPage = () => {
   return (
-    <>
+    <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
       <h2 className="text-3xl font-bold text-white mb-8 text-center">
         📬 Contact Me
       </h2>
-    </>
+      <form className="space-y-6">
+        <div>
+          <label htmlFor="name" className="block text-sm font-md text-gray-300">
+            Full Name
+          </label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-md text-gray-300"
+          >
+            Email
+          </label>
+          <input
+            id="email"
+            name="email"
+            type="email"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="subject"
+            className="block text-sm font-md text-gray-300"
+          >
+            Subject
+          </label>
+          <input
+            id="subject"
+            name="subject"
+            type="text"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="message"
+            className="block text-sm font-md text-gray-300"
+          >
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition-colors cursor-pointer"
+        >
+          Send Message
+        </button>
+      </form>
+    </div>
   );
 };
 
