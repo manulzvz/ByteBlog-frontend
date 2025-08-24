85db0cbc3c4c0ac549b9a90281568b6d3db2b16d
manulzvz13 <manulzvz@gmail.com>
2025-08-24 13:02:05 -0600

    feat: Add featured projects on homepage

--- app/routes/home/index.tsx
+++ app/routes/home/index.tsx
@@ -1,4 +1,6 @@
 import type { Route } from "./+types/index";
+import type { Project } from "~/types/project";
+import FeaturedProjects from "~/components/FeaturedProjects";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -7,6 +9,24 @@ export function meta({}: Route.MetaArgs) {
   ];
 }
 
-export default function Home() {
-  return <>Homepage</>;
+export async function loader({
+  request,
+}: Route.LoaderArgs): Promise<{ projects: Project[] }> {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/projects`);
+  const data = await res.json();
+
+  return {
+    projects: data,
+  };
 }
+
+const Homepage = ({ loaderData }: Route.ComponentProps) => {
+  const { projects } = loaderData;
+  return (
+    <>
+      <FeaturedProjects projects={projects} count={2} />
+    </>
+  );
+};
+
+export default Homepage;
